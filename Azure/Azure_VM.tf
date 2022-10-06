ff750bb4a7140f532ea594e8fc278f540ec7a12f
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-10-06 14:59:47 +0530

    Update Azure_VM.tf

--- Azure/Azure_VM.tf
+++ Azure/Azure_VM.tf
@@ -1,12 +1,79 @@
- {
- "type": "Microsoft.Insights/diagnosticSettings",
- "apiVersion": "2021-05-01-preview",
+
+  "type": "Microsoft.Compute/virtualMachines",
+  "apiVersion": "2022-03-01",
+  "name": "string",
+  "location": "string",
   "properties": {
-    "categoryGroup": "allLogs",
-    "enabled": false,
-        "metrics": 
-        {
-         "enabled": false,
+    "hardwareProfile": {
+      "vmSize": "string",
+      "vmSizeProperties": {
+        "vCPUsAvailable": "int",
+        "vCPUsPerCore": "int"
+      }
+    },
+    "osProfile": {
+      "adminPassword": "string",
+      "adminUsername": "string",
+      "allowExtensionOperations": "bool",
+      "computerName": "string",
+      "customData": "string",
+      "windowsConfiguration": {
+        "additionalUnattendContent": [
+          {
+            "componentName": "Microsoft-Windows-Shell-Setup",
+            "content": "string",
+            "passName": "OobeSystem",
+            "settingName": "string"
+          }
+        ],
+        "enableAutomaticUpdates": "bool",
+        "patchSettings": {
+          "assessmentMode": "string",
+          "automaticByPlatformSettings": {
+            "rebootSetting": "string"
+          },
+          "enableHotpatching": "bool",
+          "patchMode": "string"
+        },
+        "provisionVMAgent": "bool",
+        "timeZone": "string",
+        "winRM": {
+          "listeners": [
+            {
+              "certificateUrl": "string",
+              "protocol": "string"
+            }
+          ]
         }
+      }
+    },
+    "storageProfile": {
+      "imageReference": {
+        "id": "string",
+        "offer": "string",
+        "publisher": "string",
+        "sharedGalleryImageId": "string",
+        "sku": "string",
+        "version": "string"
+      },
+      "dataDisks": [
+        {
+          "createOption": "string",
+          "lun": "int",
+          "managedDisk": {
+            "diskEncryptionSet": {
+              "id": "string"
+            },
+            "id": "string",
+            "securityProfile": {
+              "diskEncryptionSet": {
+                "id": "string"
+              },
+              "securityEncryptionType": "string"
+            },
+            "storageAccountType": "string"
+          }
+        }
+      ]
   }
 }
