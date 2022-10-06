c36442bc8e4e82c739283ade0a256202144f1871
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-10-06 15:01:39 +0530

    Update Azure_VM.tf

--- Azure/Azure_VM.tf
+++ Azure/Azure_VM.tf
@@ -1,79 +1,9 @@
-
-  "type": "Microsoft.Compute/virtualMachines",
-  "apiVersion": "2022-03-01",
-  "name": "string",
-  "location": "string",
-  "properties": {
-    "hardwareProfile": {
-      "vmSize": "string",
-      "vmSizeProperties": {
-        "vCPUsAvailable": "int",
-        "vCPUsPerCore": "int"
-      }
-    },
-    "osProfile": {
-      "adminPassword": "string",
-      "adminUsername": "string",
-      "allowExtensionOperations": "bool",
-      "computerName": "string",
-      "customData": "string",
-      "windowsConfiguration": {
-        "additionalUnattendContent": [
-          {
-            "componentName": "Microsoft-Windows-Shell-Setup",
-            "content": "string",
-            "passName": "OobeSystem",
-            "settingName": "string"
-          }
-        ],
-        "enableAutomaticUpdates": "bool",
-        "patchSettings": {
-          "assessmentMode": "string",
-          "automaticByPlatformSettings": {
-            "rebootSetting": "string"
-          },
-          "enableHotpatching": "bool",
-          "patchMode": "string"
-        },
-        "provisionVMAgent": "bool",
-        "timeZone": "string",
-        "winRM": {
-          "listeners": [
-            {
-              "certificateUrl": "string",
-              "protocol": "string"
-            }
-          ]
-        }
-      }
-    },
-    "storageProfile": {
-      "imageReference": {
-        "id": "string",
-        "offer": "string",
-        "publisher": "string",
-        "sharedGalleryImageId": "string",
-        "sku": "string",
-        "version": "string"
-      },
-      "dataDisks": [
-        {
-          "createOption": "string",
-          "lun": "int",
-          "managedDisk": {
-            "diskEncryptionSet": {
-              "id": "string"
-            },
-            "id": "string",
-            "securityProfile": {
-              "diskEncryptionSet": {
-                "id": "string"
-              },
-              "securityEncryptionType": "string"
-            },
-            "storageAccountType": "string"
-          }
-        }
-      ]
-  }
+resource "azurerm_key_vault" "example9" {
+  name                        = "des-example-keyvault_ravi1"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "premium"
+  enabled_for_disk_encryption = false
+  purge_protection_enabled    = true
 }
