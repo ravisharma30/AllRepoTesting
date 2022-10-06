78e86b0a56b8c7e436edbb4975305b3db2826ae5
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-10-06 14:50:15 +0530

    Update Azure_VM.tf

--- Azure/Azure_VM.tf
+++ Azure/Azure_VM.tf
@@ -1,13 +1,11 @@
-resource "azurerm_managed_disk" "ravitest1" {
-  name                 = var.disk_name
-  location             = var.location
-  resource_group_name  = var.resource_group_name
-  storage_account_type = var.storage_account_type
-  create_option        = "Empty"
-  disk_size_gb         = var.disk_size_gb
-  tags = ravitesting
-  
-   encryption_settings {
-   enabled = true
- }
-}
+resource "microsoft.sql/servers/databases" "Microsoft.Insights/diagnosticSettings" {
+ "type": "Microsoft.Insights/diagnosticSettings",
+ "apiVersion": "2021-05-01-preview",
+  "properties": {
+    "categoryGroup": "allLogs",
+    "enabled": false,
+        "metrics": [
+        {
+         "enabled": false,
+        }
+  }
