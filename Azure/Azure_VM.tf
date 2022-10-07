374d731dac9b84c3f92f59ec4744f3e32ab51cbc
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-10-07 19:01:55 +0530

    Update Azure_VM.tf

--- Azure/Azure_VM.tf
+++ Azure/Azure_VM.tf
@@ -1,9 +1,29 @@
-resource "azurerm_key_vault" "example9" {
+resource "azurerm_key_vault" "testing1" {
   name                        = "des-example-keyvault_ravi1"
   location                    = azurerm_resource_group.example.location
   resource_group_name         = azurerm_resource_group.example.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "premium"
   enabled_for_disk_encryption = false
-  purge_protection_enabled    = true
+  purge_protection_enabled    = false
+}
+
+resource "azurerm_key_vault" "testing2" {
+  name                        = "des-example-keyvault_ravi1"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "premium"
+  enabled_for_disk_encryption = false
+  purge_protection_enabled    = false
+}
+
+resource "azurerm_key_vault" "testing3" {
+  name                        = "des-example-keyvault_ravi1"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "premium"
+  enabled_for_disk_encryption = false
+  purge_protection_enabled    = false
 }
