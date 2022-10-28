ac828677fd7a6fa1fa4ef491be61ea80b6793230
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-10-28 18:12:36 +0530

    Update Aws_S3_Bucket.tf

--- AWS/Aws_S3_Bucket.tf
+++ AWS/Aws_S3_Bucket.tf
@@ -6,8 +6,8 @@ resource "aws_s3_bucket" "ravidisabled" {
   bucket = "examplebuckettftest"
   acl    = "private"
   versioning {
-    enabled = true
-    mfa_delete = true
+    enabled = false
+    mfa_delete = false
   }
 }
 
