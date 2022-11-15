ccf500d4c1a5b8ed9ddfdc3de4a53fc52c25bade
ravisharma30 <107391665+ravisharma30@users.noreply.github.com>
2022-11-15 20:37:48 +0530

    Update Azure_VM.tf

--- Azure/Azure_VM.tf
+++ Azure/Azure_VM.tf
@@ -10,7 +10,7 @@ resource "azurerm_key_vault" "testing1" {
 
 
 resource "azurerm_key_vault" "testing2" {
-  name                        = "des-example-keyvault_ravi1"
+  name                        = "des-example-keyvault_ravi2"
   location                    = azurerm_resource_group.example.location
   resource_group_name         = azurerm_resource_group.example.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
@@ -20,7 +20,18 @@ resource "azurerm_key_vault" "testing2" {
 }
 
 resource "azurerm_key_vault" "testing3" {
-  name                        = "des-example-keyvault_ravi1"
+  name                        = "des-example-keyvault_ravi3"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "premium"
+  enabled_for_disk_encryption = false
+  purge_protection_enabled    = false
+  
+}
+
+resource "azurerm_key_vault" "testing4" {
+  name                        = "des-example-keyvault_ravi4"
   location                    = azurerm_resource_group.example.location
   resource_group_name         = azurerm_resource_group.example.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
